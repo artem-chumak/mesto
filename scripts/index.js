8b222d51e2523ae3adcd39c1fddf1e9055d1f90f
Artem Chumak <artem.4x@gmail.com>
2021-05-29 18:31:18 +0300

    still

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,7 @@
-//* Variables:
+//! Arrays
+
+
+//! Variables:
 
 //* BUTTON Edit Profile
 let buttonEditProfile = document.querySelector('.profile__edit-button');
@@ -17,7 +20,94 @@ let nameInput = document.querySelector('input[name="name"]');
 //* INPUT occupation
 let occupationInput = document.querySelector('input[name="occupation"]');
 
-//* Functions:
+//todo new code template
+//новый попап
+
+//* BUTTON add place
+let buttonAddPlace = document.querySelector('.profile__add-button');
+//* TITLE element
+let titleElement = document.querySelector('.element__title');
+//* IMAGE element
+let imageElement = document.querySelector('.element__image');
+//* POP-UP
+let addForm = document.querySelector('.popup-place');
+//* BUTTON close pop-up
+let closeAddForm = document.querySelector('.popup-place__exit-button');
+//* FORM
+let formAddElement = document.querySelector('.popup-place__container');
+//* INPUT name
+let titleInput = document.querySelector('input[name="place"]');
+//* INPUT occupation
+let linkInput = document.querySelector('input[name="link"]');
+
+//Тут нужно навешать еще формулы и прочее говно.
+
+
+// массив. Первые 6 объектов, когда страница загрузится.
+const initialElements = [
+  {
+    name: 'Гора Ахун',
+    link: './images/ahun.jpg'
+  },
+  {
+    name: 'Ладожское озеро',
+    link: './images/ladoga.jpg'
+  },
+  {
+    name: 'Никола-Ленивец',
+    link: './images/nikola.jpg'
+  },
+  {
+    name: 'Печоры',
+    link: './images/pechory.jpg'
+  },
+  {
+    name: 'Мыс Тобизина',
+    link: './images/tobizina.jpg'
+  },
+  {
+    name: 'Калязин',
+    link: './images/volga.jpg'
+  }
+]; 
+
+const elementTemplate = document.querySelector('.element_template'); // оъект ТЕМПЛЕЙТ
+const elementList = document.querySelector('.elements__list'); // объект UL тот, куда будем записывать ТЕМПЛЕЙТ
+
+// function renderInitialElements () {
+//   for (let index = 0; index < initialElements.length; index++) { //Конструкция FOR LOOP. 1. И=0 2. И меньше длины массива. 3. +1 к каждой новой итерации. индекс=индекс+1
+//     const title = initialElements[index].name; //сделали переменную, в которой будет имя из объекта в массиве
+//     const image = initialElements[index].link; //тоже само, но для ссылки
+
+//     elementList.insertAdjacentHTML('beforeend',`
+//     <li class="element">
+//       <img class="element__image" src="${image}" alt="Фото места">
+//       <div class="element__info">
+//         <h2 class="element__title">${title}</h2>
+//         <button class="button element__like-button" aria-label="Понравилось" type="button"></button>
+//       </div>
+//     </li>
+//     `) //тут мы поместили перед концам UL наш темплейт. те моменты, что отличались, мы заполнили данными
+//   }
+// }
+//
+//renderInitialElements () // коллбек функции. Т.к. она должна сработать при загрузки
+
+initialElements.forEach((item) => { //функция принимает три аргумента 1.Элемент массива 2.Номер элемента 3. Сам массив(редко нужно) Тут мы используем только элемент массива. Остальные не заполняли
+    elementList.insertAdjacentHTML('beforeend',`
+    <li class="element">
+      <img class="element__image" src="${item.link}" alt="Фото места">
+      <div class="element__info">
+        <h2 class="element__title">${item.name}</h2>
+        <button class="button element__like-button" aria-label="Понравилось" type="button"></button>
+      </div>
+    </li>
+    `)
+})
+
+
+
+//! Functions:
 
 //* Open/Close popup
 function toggleEditForm() {
@@ -39,7 +129,7 @@ function formSubmit(evt) {
   toggleEditForm();
 }
 
-//* Events:
+//! Events:
 
 //* CLICK button edit profile
 //? It seems that I can put all functions into one event but my attempt was failed.
